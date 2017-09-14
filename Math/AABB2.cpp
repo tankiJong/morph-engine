ca93fc282f66773d82811bc98872ebe9097ac716
Tanki Zhang <tanki_jong@hotmail.com>
2017-09-13 19:51:40 -0500

    add AABB2 to Math

--- Math/AABB2.cpp
+++ Math/AABB2.cpp
@@ -1,1 +1,91 @@
 #pragma once
+
+#include "Engine/Math/AABB2.hpp"
+
+
+
+AABB2::AABB2(const AABB2& copy)
+	: mins(copy.mins)
+	, maxs(copy.maxs) {}
+
+AABB2::AABB2(float minX, float minY, float maxX, float maxY)
+	: mins(minX, minY)
+	, maxs(maxX, maxY) {}
+
+AABB2::AABB2(const Vector2& mins, const Vector2& maxs)
+	: mins(mins)
+	, maxs(maxs) {}
+
+AABB2::AABB2(const Vector2& center, float radiusX, float radiusY)
+	: mins(center.x - radiusX, center.y - radiusY)
+	, maxs(center.x + radiusX, center.y + radiusY) {}
+
+void AABB2::stretchToIncludePoint(float x, float y) {
+	if (x < mins.x) mins.x = x;
+	else if (x > maxs.x ) maxs.x = x;
+
+	if (y < mins.y) mins.y = y;
+	else if (y > maxs.y) maxs.y = y;
+}
+
+void AABB2::stretchToIncludePoint(const Vector2& point) {
+	stretchToIncludePoint(point.x, point.y);
+}
+
+void AABB2::addPaddingToSides(float xPaddingRadius, float yPaddingRadius) {
+	Vector2 padding(xPaddingRadius, yPaddingRadius);
+
+	mins -= padding;
+	maxs += padding;
+}
+
+void AABB2::translate(const Vector2& translation) {
+	mins += translation;
+	maxs += translation;
+}
+
+void AABB2::translate(float translationX, float translationY) {
+	Vector2 translation(translationX, translationY);
+	translate(translation);
+}
+
+bool AABB2::isPointInside(float x, float y) const {
+	return x > mins.x && x < maxs.x && y > mins.y && y < maxs.y;
+}
+
+bool AABB2::isPointInside(const Vector2& point) const {
+	return isPointInside(point.x, point.y);
+}
+
+Vector2 AABB2::getDimensions() const {
+	return maxs - mins;
+}
+
+Vector2 AABB2::getCenter() const {
+	return 0.5f * (mins + maxs);
+}
+
+void AABB2::operator+=(const Vector2& translation) {
+	mins += translation;
+	maxs += translation;
+}
+
+void AABB2::operator-=(const Vector2& antiTranslation) {
+	mins -= antiTranslation;
+	maxs -= antiTranslation;
+}
+
+AABB2 AABB2::operator+(const Vector2& translation) const {
+	return AABB2(mins + translation, maxs + translation);
+}
+
+AABB2 AABB2::operator-(const Vector2& antiTranslation) const {
+	return AABB2(mins - antiTranslation, maxs - antiTranslation);
+}
+
+bool areAABBsOverlap(const AABB2& a, const AABB2& b) {
+	if (a.maxs.x < b.mins.x || b.maxs.x < a.mins.x) return false;
+	if (a.maxs.y < b.mins.y || b.maxs.y < a.mins.y) return false;
+
+	return true;
+}
--- Math/AABB2.hpp
+++ Math/AABB2.hpp
@@ -1,9 +1,33 @@
 #pragma once
+#include "Engine/Math/Vector2.hpp"
+
 class AABB2 {
 public:
-	AABB2();
-	~AABB2();
+	~AABB2() {}
+	AABB2() {}
+	AABB2(const AABB2& copy);
+	explicit AABB2(float minX, float minY, float maxX, float maxY);
+	explicit AABB2(const Vector2& mins, const Vector2& maxs);
+	explicit AABB2(const Vector2& center, float radiusX, float radiusY);
 
-private:
+	void stretchToIncludePoint(float x, float y); // note: stretch, not move…
+	void stretchToIncludePoint(const Vector2& point); // note: stretch, not move…
+	void addPaddingToSides(float xPaddingRadius, float yPaddingRadius);
+	void translate(const Vector2& translation); // move the box; similar to +=
+	void translate(float translationX, float translationY);
+          
+	bool isPointInside(float x, float y) const; // is “x,y” within box’s interior?
+	bool isPointInside(const Vector2& point) const; // is “point” within box’s interior?
+	Vector2 getDimensions() const; // return a Vector2 of ( width, height )
+	Vector2 getCenter() const; // return the center position of the box
 
+	void operator+=(const Vector2& translation); // move (translate) the box
+	void operator-=(const Vector2& antiTranslation);
+	AABB2 operator+(const Vector2& translation) const; // create a (temp) moved box
+	AABB2 operator-(const Vector2& antiTranslation) const;
+
+	Vector2 mins; // like Vector2, this breaks the “no public members” and “m_” naming rules;
+	Vector2 maxs; // this is so low-level math primitives feel like built-in types (and are fast in Debug)
 };
+
+bool areAABBsOverlap(const AABB2& a, const AABB2& b);
