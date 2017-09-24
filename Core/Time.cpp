16e49119f7986e217b68fc32de055d62f381cb7a
Tanki Zhang <tanki_jong@hotmail.com>
2017-09-24 17:58:35 -0500

    Add very simple timer

---
+++ Core/Time.cpp
@@ -0,0 +1,34 @@
+//-----------------------------------------------------------------------------------------------
+// Time.cpp
+//	
+
+//-----------------------------------------------------------------------------------------------
+#include "Engine/Core/Time.hpp"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+
+//-----------------------------------------------------------------------------------------------
+double InitializeTime( LARGE_INTEGER& out_initialTime )
+{
+	LARGE_INTEGER countsPerSecond;
+	QueryPerformanceFrequency( &countsPerSecond );
+	QueryPerformanceCounter( &out_initialTime );
+	return( 1.0 / static_cast< double >( countsPerSecond.QuadPart ) );
+}
+
+
+//-----------------------------------------------------------------------------------------------
+double GetCurrentTimeSeconds()
+{
+	static LARGE_INTEGER initialTime;
+	static double secondsPerCount = InitializeTime( initialTime );
+	LARGE_INTEGER currentCount;
+	QueryPerformanceCounter( &currentCount );
+	LONGLONG elapsedCountsSinceInitialTime = currentCount.QuadPart - initialTime.QuadPart;
+
+	double currentSeconds = static_cast< double >( elapsedCountsSinceInitialTime ) * secondsPerCount;
+	return currentSeconds;
+}
+
+
