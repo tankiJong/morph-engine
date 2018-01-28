7989cbd5fd07e2ae15a8100d174a54c1f4ba57ca
Tanki Zhang <tanki_jong@hotmail.com>
2018-01-28 12:50:57 -0600

    introduce yaml support

---
+++ Engine/Core/Yaml.cpp
@@ -0,0 +1,10 @@
+﻿#include "Yaml.hpp"
+
+#pragma comment( lib, "ThirdParty/yaml-cpp/libyaml-cppmd.lib" )
+#pragma warning( push )
+#pragma warning( disable : 4127 ) 
+#include "ThirdParty/yaml-cpp/yaml.h"
+#pragma warning( pop )
+
+YAML::Node node = YAML::Load("[1, 2, 3]");
+
