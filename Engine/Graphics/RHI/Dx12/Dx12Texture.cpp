d54d606481b16ef78605c1c62a8e3d244271cd38
Tanki Zhang <tanki_jong@hotmail.com>
2019-04-14 19:42:43 -0500

    fix error

--- Engine/Graphics/RHI/Dx12/Dx12Texture.cpp
+++ Engine/Graphics/RHI/Dx12/Dx12Texture.cpp
@@ -2,7 +2,6 @@
 #include "Engine/Graphics/RHI/Dx12/Dx12DescriptorData.hpp"
 #include "Engine/Graphics/RHI/Dx12/Dx12Resource.hpp"
 #include "Engine/Graphics/RHI/Texture.hpp"
-#include "Engine/Graphics/RHI/TiledTexture2.hpp"
 
 RHIHeap::eUsage getUsage(RHIResource::BindingFlag flags) {
   return is_set(flags, RHIResource::BindingFlag::RenderTarget | RHIResource::BindingFlag::DepthStencil) ?
@@ -223,87 +222,4 @@ RHITexture::RHITexture(rhi_resource_handle_t res): RHIResource(res) {
   mDepthOrArraySize = (uint)desc.DepthOrArraySize;
   mState.global = true;
   mState.globalState = State::Undefined;
-}
-
-bool TiledTexture2::rhiInit(bool genMipmap, const void*, size_t, bool implicitHeap) {
-TODO("`size` should be used here");
-  D3D12_RESOURCE_DESC desc = {};
-
-  if(genMipmap) {
-    uint dim = mWidth | mHeight | mDepthOrArraySize;
-    unsigned long len;
-    _BitScanReverse(&len, dim);
-    mMipLevels = len + 1u;
-  } else {
-    mMipLevels = 1;
-  }
-
-  desc.MipLevels = (uint16_t)mMipLevels;
-  desc.Format = toDXGIFormat(mFormat);
-  desc.Width = mWidth;
-  desc.Height = mHeight;
-  desc.Flags = asDx12ResourceFlags(mBindingFlags);
-  desc.SampleDesc.Count = 1;
-  desc.SampleDesc.Quality = 0;
-  desc.Dimension = D3D12_RESOURCE_DIMENSION_TEXTURE2D;
-  desc.Layout = D3D12_TEXTURE_LAYOUT_UNKNOWN;
-  desc.Alignment = 0;
-  desc.DepthOrArraySize = 1;
-
-  D3D12_CLEAR_VALUE clearValue = {};
-  D3D12_CLEAR_VALUE* clearValPtr = nullptr;
-
-  if((mBindingFlags & (Texture2::BindingFlag::RenderTarget | Texture2::BindingFlag::DepthStencil)) != Texture2::BindingFlag::None) {
-
-    clearValue.Format = desc.Format;
-    if((mBindingFlags & Texture2::BindingFlag::DepthStencil) != Texture2::BindingFlag::None) {
-      clearValue.DepthStencil.Depth = 1.f;
-    }
-    clearValPtr = &clearValue;
-  }
-
-  if(mFormat == TEXTURE_FORMAT_D24S8 && is_set(mBindingFlags, Texture2::BindingFlag::ShaderResource | Texture2::BindingFlag::UnorderedAccess)) {
-    TODO("set to typeless later when do the unorder access");
-    desc.Format = DXGI_FORMAT_R24G8_TYPELESS;
-    clearValPtr = nullptr;
-  }
-
-
-  auto device = RHIDevice::get()->nativeDevice();
-  d3d_call(device->CreateReservedResource(&desc, D3D12_RESOURCE_STATE_COMMON, clearValPtr, IID_PPV_ARGS(&mRhiHandle)));
-
-
-  mState.subresourceState.resize(mMipLevels * mDepthOrArraySize, mState.globalState);
-  mState.subresourceInTransition.resize(mMipLevels * mDepthOrArraySize, mState.globalInTransition);
-
-  mRhiHandle->SetName(L"Tiled Texture2");
-
-  //if(genMipmap) {
-  //  generateMipmap(*RHIDevice::get()->defaultRenderContext());
-  //}
-
-  return true;
-}
-
-void TiledTexture2::tile(uint x, uint y, Texture2::sptr_t& tex) {
-	EXPECTS(tex->width() == mTileWidth && tex->height() == mTileHeight);
-  EXPECTS(tex->mipCount() == mipCount());
-	tile(x, y) = tex;
-
-  uint tileMapCount = mipCount();
-
-  D3D12_TILED_RESOURCE_COORDINATE* tileCoords 
-    = (D3D12_TILED_RESOURCE_COORDINATE*)_alloca(sizeof(D3D12_TILED_RESOURCE_COORDINATE) * tileMapCount);
-  D3D12_TILE_REGION_SIZE* tileRegionSize
-    = (D3D12_TILE_REGION_SIZE*)_alloca(sizeof(D3D12_TILE_REGION_SIZE) * tileMapCount);
-  for(uint i = 0; i < tileMapCount; i++) {
-    tileCoords[i].X = 0;
-    tileCoords[i].Y = 0;
-    tileCoords[i].Z = 0;
-    tileCoords[i].Subresource = i;
-    //tileRegionSize[i].
-  }
-	//RHIDevice::get()->defaultRenderContext()->contextData()
-	//								->commandQueue()
-	//								->UpdateTileMappings(mRhiHandle.Get(), 1, )
 }
